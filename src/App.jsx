5a58a697b983f291b0cc85a6885e0b125371ba5d
AungThuraHein <u6726135@au.edu>
2025-12-19 16:22:03 +0700

    delete unnecessary imports(Not used yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 
 import './App.css'
-import Profile from './components/Profile'
 import { Route, Routes } from 'react-router-dom'
-import Register from './components/Register'
-import List from './components/List'
 import Greetings from './components/Greetings'
-import User from './components/User'
 
 function App() {
   return (
